0788db3b8c3c6171c03bc289834855ee97e33d73
Chien-Yu Lin <cy20lin@gmail.com>
2020-02-24 11:12:32 +0800

    Solve the noise_dim default value issue while training
    and update scripts to be executable as well

--- sgan/models.py
+++ sgan/models.py
@@ -421,11 +421,11 @@ class TrajectoryGenerator(nn.Module):
                 neighborhood_size=neighborhood_size,
                 grid_size=grid_size
             )
-
-        if self.noise_dim[0] == 0:
-            self.noise_dim = None
-        else:
-            self.noise_first_dim = noise_dim[0]
+        if self.noise_dim != None:
+            if self.noise_dim[0] == 0:
+                self.noise_dim = None
+            else:
+                self.noise_first_dim = noise_dim[0]
 
         # Decoder Hidden
         if pooling_type:
